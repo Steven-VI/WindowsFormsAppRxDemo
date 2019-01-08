07d94b4a059b1ecc57790228321737a45e8cc94e
Steven Van Iseghem <steven.van.iseghem@outlook.com>
2019-01-08 19:35:19 +0100

    Adding Schedulers in Rx

--- WindowsFormsAppRx/Form1.cs
+++ WindowsFormsAppRx/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,15 @@ namespace WindowsFormsAppRx
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Using schedulers in Rx
             var searchTerm = Observable.FromEventPattern<EventArgs>(
-                textBox1, "TextChanged").Select(x => ((TextBox)x.Sender).Text).Where(text => text.EndsWith("."));
+                textBox1, "TextChanged").Select(x => ((TextBox)x.Sender).Text).Throttle(TimeSpan.FromMilliseconds(5000));
 
-            searchTerm.Subscribe(trm => label1.Text = trm);
+            //searchTerm.Subscribe(trm => label1.Text = trm);
+            /// View - Other Windows - Package Manager Console
+            /// Install-Package System.Reactive.Windows.Forms
+
+            searchTerm.ObserveOn(new ControlScheduler(this)).Subscribe(trm => label1.Text = trm);
 
             // Linq aspect Rx
             /// Observable.Empty<>: Empty observable sequence
